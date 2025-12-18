a2f657a5e2f2c969f4cd0db257428b89fe7c4a52
bala-amypo <bala@amypo.in>
2025-12-18 10:37:15 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/ShipmentServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/ShipmentServiceImpl.java
@@ -1,52 +1,38 @@
 package com.example.demo.service.impl;
 
+import org.springframework.stereotype.Service;
 import com.example.demo.entity.Shipment;
 import com.example.demo.entity.Vehicle;
-import com.example.demo.exception.ResourceNotFoundException;
-import com.example.demo.repository.ShipmentRepo;
-import com.example.demo.repository.VehicleRepo;
+import com.example.demo.repository.ShipmentRepository;
+import com.example.demo.repository.VehicleRepository;
 import com.example.demo.service.ShipmentService;
-import org.springframework.stereotype.Service;
-
-import java.time.LocalDate;
 
 @Service
 public class ShipmentServiceImpl implements ShipmentService {
 
-    private final ShipmentRepo repo;
-    private final VehicleRepo vehicleRepo;
+    private final ShipmentRepository shipmentRepository;
+    private final VehicleRepository vehicleRepository;
 
-    public ShipmentServiceImpl(ShipmentRepo repo, VehicleRepo vehicleRepo) {
-        this.repo = repo;
-        this.vehicleRepo = vehicleRepo;
+    public ShipmentServiceImpl(ShipmentRepository shipmentRepository,
+                               VehicleRepository vehicleRepository) {
+        this.shipmentRepository = shipmentRepository;
+        this.vehicleRepository = vehicleRepository;
     }
 
     @Override
     public Shipment create(Long vehicleId, Shipment shipment) {
-
-        Vehicle vehicle = vehicleRepo.findById(vehicleId)
-                .orElseThrow(() -> new ResourceNotFoundException("Vehicle not found"));
-
-        if (shipment.getWeightKg() > vehicle.getCapacityKg()) {
-            throw new IllegalArgumentException("Weight exceeds capacity");
-        }
-
-        if (shipment.getScheduledDate().isBefore(LocalDate.now())) {
-            throw new IllegalArgumentException("Date in the past");
-        }
-
+        Vehicle vehicle = vehicleRepository.findById(vehicleId).orElse(null);
         shipment.setVehicle(vehicle);
-        return repo.save(shipment);
+        return shipmentRepository.save(shipment);
     }
 
     @Override
     public Shipment get(Long id) {
-        return repo.findById(id)
-                .orElseThrow(() -> new ResourceNotFoundException("Shipment not found"));
+        return shipmentRepository.findById(id).orElse(null);
     }
 
     @Override
     public void delete(Long id) {
-        repo.delete(get(id));
+        shipmentRepository.deleteById(id);
     }
 }
