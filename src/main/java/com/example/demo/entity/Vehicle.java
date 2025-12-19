b5bba0c94401decb1fd0e80afd13cc4d0bd2cff3
bala-amypo <bala@amypo.in>
2025-12-19 09:34:21 +0000

    User commit

--- src/main/java/com/example/demo/entity/Vehicle.java
+++ src/main/java/com/example/demo/entity/Vehicle.java
@@ -3,6 +3,7 @@ package com.example.demo.entity;
 import jakarta.persistence.*;
 
 @Entity
+@Table(name = "vehicle")
 public class Vehicle {
 
     @Id
@@ -13,4 +14,27 @@ public class Vehicle {
     private String type;
 
     // getters & setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getVehicleNumber() {
+        return vehicleNumber;
+    }
+
+    public void setVehicleNumber(String vehicleNumber) {
+        this.vehicleNumber = vehicleNumber;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
 }
