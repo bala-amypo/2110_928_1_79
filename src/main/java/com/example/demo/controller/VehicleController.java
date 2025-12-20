e89d136b449daad9c6408587a616e6ee38055d23
bala-amypo <bala@amypo.in>
2025-12-20 17:17:43 +0000

    User commit

--- src/main/java/com/example/demo/controller/VehicleController.java
+++ src/main/java/com/example/demo/controller/VehicleController.java
@@ -2,42 +2,27 @@ package com.example.demo.controller;
 
 import com.example.demo.entity.Vehicle;
 import com.example.demo.service.VehicleService;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 
 @RestController
 @RequestMapping("/vehicles")
 public class VehicleController {
 
-    @Autowired
-    private VehicleService vehicleService;
+    private final VehicleService vehicleService;
 
-    // Get vehicles by user
-    
-    @GetMapping("/user/{userId}")
-    public List<Vehicle> getVehiclesByUser(
-            @Parameter(description = "ID of the user to fetch vehicles for", required = true)
-            @PathVariable Long userId) {
-        return vehicleService.getVehiclesByUser(userId);
+    public VehicleController(VehicleService vehicleService) {
+        this.vehicleService = vehicleService;
     }
 
-    // Add a new vehicle
-    
     @PostMapping("/{userId}")
-    public Vehicle addVehicle(
-            @PathVariable Long userId,
-            @RequestBody Vehicle vehicle) {
+    public Vehicle addVehicle(@PathVariable Long userId,
+                              @RequestBody Vehicle vehicle) {
         return vehicleService.addVehicle(userId, vehicle);
     }
 
-    // Get vehicle by ID
-   
-    @GetMapping("/id/{id}")
-    public Vehicle getVehicleById(@PathVariable Long id) {
-        return vehicleService.findById(id);
+    @GetMapping("/user/{userId}")
+    public List<Vehicle> getVehicles(@PathVariable Long userId) {
+        return vehicleService.getVehiclesByUser(userId);
     }
 }
