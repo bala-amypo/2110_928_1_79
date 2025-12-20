1f2d0a8250f3bc5401e40fa4a9771fa0ff2b60c0
bala-amypo <bala@amypo.in>
2025-12-20 16:08:43 +0000

    User commit

--- src/main/java/com/example/demo/service/VehicleService.java
+++ src/main/java/com/example/demo/service/VehicleService.java
@@ -1,19 +1,8 @@
 package com.example.demo.service;
 
 import com.example.demo.entity.Vehicle;
-import com.example.demo.repository.VehicleRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 import java.util.List;
 
-@Service
-public class VehicleService {
-
-    @Autowired
-    private VehicleRepository vehicleRepository;
-
-    // Fetch vehicles by userId
-    public List<Vehicle> getVehiclesByUserId(Long userId) {
-        return vehicleRepository.findByUserId(userId);
-    }
+public interface VehicleService {
+    List<Vehicle> getVehiclesByUserId(Long userId);
 }
