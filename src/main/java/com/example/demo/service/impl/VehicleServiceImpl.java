3e27ae6da428261a64335c7d5ad187c44e36f34e
bala-amypo <bala@amypo.in>
2025-12-20 16:06:47 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/VehicleServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/VehicleServiceImpl.java
@@ -1,25 +1,20 @@
 package com.example.demo.service.impl;
 
 import com.example.demo.entity.Vehicle;
+import com.example.demo.repository.VehicleRepository;
 import com.example.demo.service.VehicleService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class VehicleServiceImpl implements VehicleService {
 
-    private final List<Vehicle> vehicles = new ArrayList<>();
+    @Autowired
+    private VehicleRepository vehicleRepository;
 
     @Override
-    public List<Vehicle> getAllVehicles() {
-        return vehicles;
-    }
-
-    @Override
-    public Vehicle saveVehicle(Vehicle vehicle) {
-        vehicles.add(vehicle);
-        return vehicle;
+    public List<Vehicle> getVehiclesByUserId(Long userId) {
+        return vehicleRepository.findByUserId(userId);
     }
 }
